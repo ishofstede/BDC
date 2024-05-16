b3e2dc5399a78a4943cb7a92bd032009be51443a
ishofstede <i.s.hofstede@st.hanze.nl>
2024-05-16 11:43:20 +0200

    functions in order

--- Assignment1/assignment1.py
+++ Assignment1/assignment1.py
@@ -35,12 +35,17 @@ def iterate_fastqfile():
         return [(phredscore / (counter / 4)) for phredscore in results]
 
 
+def main():
+    argparser = ap.ArgumentParser(description="Script voor Opdracht 1 van Big Data Computing")
+    argparser.add_argument("-n", action="store",
+                           dest="n", required=True, type=int,
+                           help="Aantal cores om te gebruiken.")
+    argparser.add_argument("-o", action="store", dest="csvfile", type=ap.FileType('w', encoding='UTF-8'),
+                           required=False, help="CSV file om de output in op te slaan. Default is output naar terminal STDOUT")
+    argparser.add_argument("fastq_files", action="store", type=ap.FileType('r'), nargs='+', help="Minstens 1 Illumina Fastq Format file om te verwerken")
+    args = argparser.parse_args()
+    iterate_fastqfile()
 
-argparser = ap.ArgumentParser(description="Script voor Opdracht 1 van Big Data Computing")
-argparser.add_argument("-n", action="store",
-                       dest="n", required=True, type=int,
-                       help="Aantal cores om te gebruiken.")
-argparser.add_argument("-o", action="store", dest="csvfile", type=ap.FileType('w', encoding='UTF-8'),
-                       required=False, help="CSV file om de output in op te slaan. Default is output naar terminal STDOUT")
-argparser.add_argument("fastq_files", action="store", type=ap.FileType('r'), nargs='+', help="Minstens 1 Illumina Fastq Format file om te verwerken")
-args = argparser.parse_args()
+
+if __name__ == '__main__':
+    main()
